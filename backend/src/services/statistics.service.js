ff13f5c00db57cc86b551498bf5b2eb9f8ca1f6a
WaiYanKyaw <waiyankyaw.dev@gmail.com>
2025-04-16 00:59:10 +0800

    helper functions for streak is created

--- backend/src/services/statistics.service.js
+++ backend/src/services/statistics.service.js
@@ -5,4 +5,24 @@ const statisticsService = {
 
 }
 
+  function getWeekNumber(date) {
+    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+  }
+  
+  function parseWeekKey(weekKey) {
+    const [year, weekPart] = weekKey.split('-');
+    const week = parseInt(weekPart.substring(1), 10);
+    return [parseInt(year, 10), week];
+  }
+  
+  function getWeeksDifference(year1, week1, year2, week2) {
+    const weeks1 = year1 * 52 + week1;
+    const weeks2 = year2 * 52 + week2;
+    return Math.abs(weeks2 - weeks1);
+  }
+
 module.exports = { statisticsService };
