a5b9d14c61bfe6a396d6d18bc53b13d23c9c90a6
Nplus4 <kevin.evan.ko.0523@gmail.com>
2025-04-17 00:42:11 +0800

    Readjust the CSS Settings

--- frontend/src/components/bar-chart.tsx
+++ frontend/src/components/bar-chart.tsx
@@ -64,8 +64,8 @@ export default function AnnualBarChart({ chartData = [] }: ComponentProps) {
   }, [selectedYear, chartData]);
 
   return (
-    <div className="mx-auto">
-      <Card>
+    <div className="mx-auto w-full max-w-5xl">
+      <Card className="bg-background shadow-none border-0">
         <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
           <div className="grid flex-1 gap-1 text-center sm:text-left">
             <CardTitle>Total Workouts Completed Per Month</CardTitle>
