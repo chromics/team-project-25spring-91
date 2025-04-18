39bff4cfa79b36600e8fb64ac8d2b1c6181fd731
Nplus4 <kevin.evan.ko.0523@gmail.com>
2025-04-19 00:05:39 +0800

    feat: add Tabs component to Competition Page and required dependencies

--- frontend/src/app/dashboard/competition/page.tsx
+++ frontend/src/app/dashboard/competition/page.tsx
@@ -1,11 +1,57 @@
 "use client";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+const yourRank = 1;
 
 const CompetitionPage = () => {
     return (
-        <div>
-            Hello
+        <div className="container mx-auto px-4 py-6">
+            <Tabs defaultValue="volume" className="w-full">
+                <div>
+                    <TabsList>
+                        <TabsTrigger value="volume">Volume-based</TabsTrigger>
+                        <TabsTrigger value="streak">Streak-based</TabsTrigger>
+                    </TabsList>
+                </div>
+                <TabsContent value="volume">
+                    <Card className="w-full">
+                        <CardHeader>
+                            <CardTitle className="text-center">Leaderboards</CardTitle>
+                            <CardDescription>Your rank: {yourRank}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p>Volume</p>
+                        </CardContent>
+                        <CardFooter>
+                            <p>Card Footer</p>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="streak">
+                    <Card className="w-full">
+                        <CardHeader>
+                            <CardTitle className="text-center">Leaderboards</CardTitle>
+                            <CardDescription>Your rank: {yourRank}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p>Streak</p>
+                        </CardContent>
+                        <CardFooter>
+                            <p>Card Footer</p>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
         </div>
     );
 };
